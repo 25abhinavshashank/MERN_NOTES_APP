734b45204dde996eeca82dfe807065036ad4e8ec
Abhinav Shashank <shashankabhinav6@gmail.com>
2026-04-17 13:00:45 +0530

    change the file name

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,29 @@
 require('dotenv').config();
 
-const express=require('express')
-const mongoose=require('mongoose')
-const app=express();
-const appRouter=require("../src/routes/notesRoutes");
-const cors=require('cors')
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+const appRouter = require("../src/routes/notesRoutes");
+const cors = require('cors')
 
 app.use(cors())
 app.use(express.json());
-app.use("/notes",appRouter);
+app.use("/notes", appRouter);
 
 
 
 
-const dns=require('dns');
-dns.setServers(["8.8.8.8","0.0.0.0"])
+const dns = require('dns');
+dns.setServers(["8.8.8.8", "0.0.0.0"])
 
 
-const initialise_connection=async()=>{
-      try {
+const initialise_connection = async () => {
+    try {
         await mongoose.connect(process.env.DB_URL);
         console.log("Db connected");
 
-        const PORT = process.env.PORT || 5000;
-        app.listen(PORT, () => {
-            console.log("App listening at port no :" + PORT);
+        app.listen(process.env.PORT, () => {
+            console.log("App listening at port no :" + process.env.PORT);
         });
 
     } catch (err) {
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './header'
+import Header from './Header'
 import { Link, Outlet } from 'react-router-dom'
 
 function Layout() {
